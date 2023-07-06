da59606067d328abbca742e975f7802f5f13164b
bintadam <zakiyaadan12@gmail.com>
2023-07-06 17:31:58 +0200

    add jquery

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const challenges = document.querySelector
+const hobbies  =[
+    "💻 Programmer",
+    "🌱 Educator",
+    "📖 Instructor",
+    "🔥 Motivator",
+    "🔥 Motivational speaker",
+    "📙 Content Creator",
+]
+
+const tecnologies = [
+    "React",
+    "Redux",
+    "MONGODB",
+    "JAVASCRIPT",
+    "NODE",
+    "PYTHON",
+    "PANDAS",
+    "HTML",
+    "NUMPY",
+]
+
+$(document).ready(function(){
+    $(".list").html("<p></p>")
+})
