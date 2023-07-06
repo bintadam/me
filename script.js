ed92a2809f64d304955df607b1adea5407b47fe7
bintadam <zakiyaadan12@gmail.com>
2023-07-06 20:30:08 +0200

    add function

--- script.js
+++ script.js
@@ -19,6 +19,25 @@ const tecnologies = [
     "NUMPY",
 ]
 
+function animateHobbies(){
+    let index = 0;
+    const container = $(".list");
+
+    function displayHobby(){
+        container.fadeOut(400, function(){
+            container.text(hobbies[index]);
+        });
+
+        index++;
+        if(index === hobbies.length){
+            index =0;
+        }
+    }
+
+    displayHobby();
+    setInterval(displayHobby, 2000)
+}
+
 $(document).ready(function(){
-    $(".list").html("<p></p>")
+    animateHobbies();
 })
